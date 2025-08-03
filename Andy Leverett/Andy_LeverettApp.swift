ab55ba9e457ab3710e44c9e05e8c810bea9acc66
Andy Leverett <65052696+visionarycoding@users.noreply.github.com>
2025-08-03 18:24:54 -0400

    Add files via upload

---
+++ Andy Leverett/Andy_LeverettApp.swift
@@ -0,0 +1,17 @@
+//
+//  Andy_LeverettApp.swift
+//  Andy Leverett
+//
+//  Created by Andy Leverett on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Andy_LeverettApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Andy Leverett/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Andy Leverett
+//
+//  Created by Andy Leverett on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Andy's first iOS app, coming soon!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
